54683ee6bc2e8453d76266505b50f9426ca839f2
Ashis <ashish.saha@codeclouds.in>
2023-07-08 01:14:17 +0530

    Customize the Dashbard and structured the files

---
+++ admin/sidebarmenu.php
@@ -0,0 +1,51 @@
+<aside class="sidebar navbar-default" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="index.html" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-shopping-cart fa-fw"></i>Manage Product<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                            <li>
+                                    <a href="morris.html">All Products</a>
+                                </li>
+                                <li>
+                                    <a href="flot.html">Add Products</a>
+                                </li>
+                                
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-cart-plus fa-fw"></i> Manage Order<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="flot.html">All Orders</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-users fa-fw"></i>Manage Users<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+
+                                <li>
+                                    <a href="morris.html">All Users</a>
+                                </li>
+                                <li>
+                                    <a href="morris.html">Add Users</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="tables.html"><i class="fa fa-gear fa-fw"></i> Site Settings</a>
+                        </li>
+                        <li>
+                            <a href="forms.html"><i class="fa fa-power-off fa-fw"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+            
