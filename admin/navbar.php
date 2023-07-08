a5a2e30f218cc3c7dd7a3fcac4780b6bcfd0fa68
Ashis <ashish.saha@codeclouds.in>
2023-07-08 17:49:04 +0530

    Updated the layouts of admin navbar and sidebar

--- admin/navbar.php
+++ admin/navbar.php
@@ -15,9 +15,9 @@
                             <i class="fa fa-user fa-fw"></i> secondtruth <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu dropdown-user">
-                            <!-- <li>
-                                <a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                            </li> -->
+                            <li>
+                                <a href="#"><i class="fa fa-user fa-fw"></i> Admin Profile</a>
+                            </li>
                             <li>
                                 <a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
                             </li>
--- admin/sidebarmenu.php
+++ admin/sidebarmenu.php
@@ -1,6 +1,18 @@
 <aside class="sidebar navbar-default" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
+                    <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-primary" type="button">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                                <!-- <h2>Dashboard</h2> -->
+                            </div>
+                            <!-- /input-group -->
+                        </li>
                         <li>
                             <a href="index.html" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                         </li>
